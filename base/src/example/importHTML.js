a290d2e006a1ddfb05792c5d90e0ed0ba576e8d0
xiemy <4385223434@qq.com>
2021-02-01 17:42:24 +0800

    fix

--- base/src/example/importHTML.js
+++ base/src/example/importHTML.js
@@ -1,27 +1,24 @@
 import importHTML from 'import-html-entry';
 
 
-// 子应用沙箱原理
+
+const rawWindow = window
+const addWindowKeyMap = new Set()
+
+
+/***
+ *  将子应用代码的执行上下文环境绑定为proxy
+ * 
 function getExecutableScript(script, proxy, strictGlobal= true) {
   
     var globalWindow = (0, eval)('window');
     globalWindow.proxy = proxy;
 
-    // 将子应用代码的执行上下文环境绑定为proxy
     return `;(function(window,  self, globalThis){
         ${script}
      }).bind(window.proxy)(window.proxy, window.proxy, window.proxy);`
   }
 
-  // proxy 代理window
-  const proxy = new Proxy(window, {
-    get: function(target, key) {
-        console.log('---get---', key)
-    },
-    set: function(target, key) {
-        console.log('---set---', key)
-    }
-})
 
 // 'window.b = 123;' 从子应用获取的字符串代码
 var code = getExecutableScript('window.b = 123;', proxy);
@@ -29,10 +26,34 @@ var code = getExecutableScript('window.b = 123;', proxy);
 // eval 执行子应用代码
 (0, eval)(code);
 
+ */
 
-importHTML('http://127.0.0.1:8082')
-    .then(res => {
-        res.execScripts().then(exports => {
-          console.log(exports)
-    })
-});
+
+// 用proxy代理window，传给子应用，子应用对全局属性的操作就是对该 proxy 对象属性的操作。
+const proxy = new Proxy(window, {
+    get: function (target, key) {
+        console.log('---get---', key)
+        return rawWindow[key]
+    },
+    set: function (target, key, value) {
+        console.log('---set---', key)
+        addWindowKeyMap.add(key)
+        rawWindow[key] = value
+    }
+})
+
+window.addEventListener('popstate', function (a, b) {
+    if (window.location.pathname === '/app1') {
+        importHTML('http://127.0.0.1:8083').then(res => {
+            // 执行子应用代码
+            res.execScripts(proxy).then(exports => {
+                console.log(exports)
+            })
+        });
+    } else {
+        // 切换到主应用时删除在子应用添加的window属性
+        for (let key of addWindowKeyMap.keys()) {
+            delete rawWindow[key]
+        }
+    }
+})
